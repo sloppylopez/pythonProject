0076a19e84b9b241226ee2248b3f34757bff250e
sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>
2021-12-23 18:48:40 +0100

    fix plotting not showing

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
-from six.moves import urllib
-
+# from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
 
-import tensorflow as tf
-# %tensorflow_version 2.x
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import tensorflow as tf  # now import the tensorflow module
 
-def print_hi(name):
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():  # inner function, this will be returned
+        ds = tf.data.Dataset.from_tensor_slices(
+            (dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+        if shuffle:
+            ds = ds.shuffle(1000)  # randomize order of data
+        ds = ds.batch(batch_size).repeat(
+            num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+        return ds  # return a batch of the dataset
+
+    return input_function  # return a function object for use
+
+def calculateLinearRegreassion(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print(tf.version)  # make sure the version is 2.x
@@ -39,18 +49,6 @@ def print_hi(name):
 
     print(feature_columns)
 
-    def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-        def input_function():  # inner function, this will be returned
-            ds = tf.data.Dataset.from_tensor_slices(
-                (dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
-            if shuffle:
-                ds = ds.shuffle(1000)  # randomize order of data
-            ds = ds.batch(batch_size).repeat(
-                num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
-            return ds  # return a batch of the dataset
-
-        return input_function  # return a function object for use
-
     train_input_fn = make_input_fn(dftrain,
                                    y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
     eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
@@ -59,15 +57,20 @@ def print_hi(name):
     result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
 
     clear_output()  # clears console output
-    print(f'result {result["accuracy"]}')
+    print(f'accuracy of the result {result["accuracy"]}')
+    pred_dicts = list(linear_est.predict(eval_input_fn))
+    probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
+    print(probs)
+    probs.plot(kind='hist', bins=20, title='predicted probabilities')
+    plt.show()
     # print(dftrain.shape)
     # print(dftrain.describe)
     # print(dftrain.head)
     # print(y_train.head)
     # print(y_eval.head)
-    # print(dftrain.age.hist(bins=20))
-    # print(dftrain.sex.value_counts().plot(kind='barh'))
+    # dftrain.age.hist(bins=20)
+    # dftrain.sex.value_counts().plot(kind='barh')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    calculateLinearRegreassion('PyCharm')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
