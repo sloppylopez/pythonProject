6d7882a70f92f300dcb0afafb3e5a78215f2faf7
sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>
2022-01-08 16:56:46 +0100

    feature: added jupiter notebook as playground

--- display.py
+++
@@ -1,5 +0,0 @@
-from IPython.display import HTML
-
-# Youtube
-HTML('<iframe width="560" height="315" src="https://www.youtube.com/watch?v=wQ8BIBpya2k" frameborder="0" allowfullscreen></iframe>')
-HTML('<iframe width="560" height="315" src="https://www.youtube.com/watch?v=qFJeN9V1ZsI" frameborder="0" allowfullscreen></iframe>')
